88077015592ff81c0df16c5341000447b95abcaf
Oleksandr Astashov <oleksandr_astashov@ext.airbnb.com>
2021-05-04 15:59:23 -0700

    add tests for pool sorter

--- tests/mempool.c
+++ tests/mempool.c
@@ -2,6 +2,58 @@
 
 #include "../main.h"
 
+// qsort() + track_block_sort() as comparator
+
+void track_block_sort_test(void)
+{
+    size_t blocks_len = 16;
+
+    void* unordered_blocks[16] = {
+        NULL,
+        (void*) 0x7fecc84128b2,
+        (void*) 0x7fecc841288a,
+        NULL,
+        (void*) 0x7fecc8412812,
+        NULL,
+        (void*) 0x7fecc841289a,
+        (void*) 0x7fecc84128a2,
+        NULL,
+        (void*) 0x7fecc87128aa,
+        NULL,
+        NULL,
+        (void*) 0x7fecc84128dd,
+        (void*) 0x7fecc84128ce,
+        NULL,
+        (void*) 0x7fecc84118cd
+    };
+    void* ordered_blocks[16] = {
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        (void*) 0x7fecc84118cd,
+        (void*) 0x7fecc8412812,
+        (void*) 0x7fecc841288a,
+        (void*) 0x7fecc841289a,
+        (void*) 0x7fecc84128a2,
+        (void*) 0x7fecc84128b2,
+        (void*) 0x7fecc84128ce,
+        (void*) 0x7fecc84128dd,
+        (void*) 0x7fecc87128aa,
+    };
+
+    print_table_content((void **) &unordered_blocks, blocks_len - 1);
+    qsort(unordered_blocks, blocks_len, sizeof (void *), track_block_sort);
+    print_table_content((void **) &unordered_blocks, blocks_len - 1);
+
+    for (size_t i = 0; i < blocks_len; ++i) {
+        assert(unordered_blocks[i] == ordered_blocks[i]);
+    }
+}
+
 // safe_alloc()
 void safe_alloc_free_test(void) {
     uint16_t* block_16 = safe_alloc(16);
@@ -72,6 +124,7 @@ int main(int argc, char** argv)
 {
     debug(DEBUG_INFO, "Mempool test starting... %c", '\n');
 
+    track_block_sort_test();
     safe_alloc_free_test();
     track_block_test();
 
